c143d4b46967a7b02c04d05e1c3f50256eb7d2f5
Edmund Lai <edmund.lai.07@gmail.com>
2019-09-13 23:01:28 -0700

    Added test blog posts to create blog website structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Food Blog</h1>
       </header>
+      <main>
+        <div className="BodyContainer">
+          <PostsContainer />
+        </div>
+      </main>
+      <footer className="App-footer">
+        <FooterInfo />
+      </footer>
     </div>
   );
+}
+
+// Container for all the blog posts that will be created
+class PostsContainer extends React.Component {
+  render() {
+    return (
+      <div className="PostsContainer">
+        <BlogPost />
+        <BlogPost />
+      </div>
+    );
+  }
+}
+
+class BlogPost extends React.Component {
+  render () {
+    return (
+      <div className="BlogPost">
+        <PostTopline />
+        <PostArticle />
+      </div>
+    );
+  }
+}
+
+class PostTopline extends React.Component {
+  render() {
+    return (
+      <div className="PostTopline">
+        <h2>Post Title</h2>
+        <ArticleInfo />
+      </div>
+    );
+  }
+}
+
+class ArticleInfo extends React.Component {
+  render() {
+    return (
+      <div className="ArticleInfo">
+        <div>Post Author</div>
+        <div>Date Posted</div>
+      </div>
+    );
+  }
+}
+
+class PostArticle extends React.Component {
+  render() {
+    return (
+      <div className="PostArticle">
+        Post Content
+      </div>
+    );
+  };
+}
+
+class FooterInfo extends React.Component {
+  render() {
+    return (
+      <div className="FooterInfo">
+        &#169; Test User 2019
+      </div>
+    );
+    
+  }
 }
 
 export default App;
